332a813076065254165a3173eef4aa91ee51090e
Sergio Martí Torregrosa <sergio.martihee1997@gmail.com>
2021-10-18 17:08:45 +0200

    Clase 18/10/2021

---
+++ problemas10182021.py
@@ -0,0 +1,32 @@
+# APUNTES DÍA: 18/10/2021
+
+from mis_subrutinas import *
+
+#a = int(input("Introduce el valor de a: "))
+#b = int(input("Introduce el valor de b: "))
+#c = int(input("Introduce el valor de c: "))
+
+#a = 10
+#b = 12
+#c = 4
+
+#print(Max3(a, b, c))
+#print(Max2(a, b))
+
+# Listas
+
+# Las listas son contenedores de elementos, ordenados por un indice.
+# En python, las listas comienzan con índice 0
+
+# definir la lista
+bases = ['A', 'C', 'G', 'T']
+
+# mostrar todas las bases
+print(bases)
+
+# mostrar el elemento con índice 2
+print(bases[2])
+
+# mostrar una sublista de la lista
+# 
+print(bases[1:4])
